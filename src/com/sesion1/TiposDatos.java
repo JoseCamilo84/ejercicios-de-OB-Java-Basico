d0ea1db86d2b56758b16cce63cba792e3557b0bf
Jose Camilo <jrodriguez_131@hotmail.com>
2022-07-21 21:49:05 -0500

    Primer commit

---
+++ src/com/sesion1/TiposDatos.java
@@ -0,0 +1,34 @@
+package com.sesion1;
+
+public class TiposDatos {
+    public static void main(String[] args) {
+        // enteros
+        byte numero1 = 4;
+        short numero2 = 6;
+        int numero3 = 45;
+        long numero4 = 125;
+        float numero5 = 56.87f;
+        double numero6 = 452.26;
+
+        System.out.println(numero1);
+        System.out.println(numero2);
+        System.out.println(numero3);
+        System.out.println(numero4);
+        System.out.println(numero5);
+        System.out.println(numero6);
+
+        // booleanos
+        boolean palabra1 = false;
+        boolean palabra2 = true;
+
+        System.out.println(palabra1);
+        System.out.println(palabra2);
+
+        // char y String
+        char caracter = 'e';
+        String nombre = "José Camilo";
+
+        System.out.println(caracter);
+        System.out.println(nombre);
+    }
+}
